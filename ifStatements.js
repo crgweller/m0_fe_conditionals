f6e23e63b8859ab51c717e50a2fd208992a606bd
Craig Weller <crg.weller@gmail.com>
2022-09-21 20:30:31 -0600

    See if I broke it

--- ifStatements.js
+++ ifStatements.js
@@ -68,3 +68,16 @@ console.log("I don't have enough money for a gumball");
 
 var cupsOfFlour = 1;
 var hasSauce = true;
+
+if (cupsOfFlour === 1 && hasSauce === true) {
+  console.log("I cannot make pizza")
+}
+else if (cupsOfFlour === 5 && hasSauce === false) {
+  console.log("I cannot make pizza")
+}
+else if (cupsOfFlour === 2 && hasSauce === true) {
+  console.log("I can make pizza")
+}
+else if (cupsOfFlour === 3 && hasSauce === true) {
+  console.log("I can make pizza")
+}
